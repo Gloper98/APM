04e04fb18065425d9a5f5a4522f5d534319552d4
Gloper98 <littleannygutlop_2014@outlook.es>
2018-04-21 19:45:38 -0500

    Updating star component and nested functionality

---
+++ src/app/shared/convert-to-spaces.pipe.ts
@@ -0,0 +1,9 @@
+import { Pipe } from '@angular/core';
+@Pipe({
+    name: 'convertToSpaces'
+})
+export class ConvertToSpacesPipe {
+    transform(value:string, character: string): string {
+        return value.replace(character, ' ');
+    }
+}
